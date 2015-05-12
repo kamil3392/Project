14e0af143b4f66c6870aa3f1e2b94a2b7f3ef006
Kamil Kapturski <kamil3392@o2.pl>
2015-05-12 19:26:52 +0000

    add option delete project

--- myapp/myapp/app/controllers/articles_controller.rb
+++ myapp/myapp/app/controllers/articles_controller.rb
@@ -22,6 +22,13 @@ def create
   end
 end
  
+def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+ 
+    redirect_to articles_path
+end
+  
 private
   def article_params
     params.require(:article).permit(:title, :text)
