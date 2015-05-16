2951c3f9a14f08b115a973e7ecaaec599d58b59b
Kamil Kapturski <kamil3392@o2.pl>
2015-05-16 10:37:55 +0000

    add references between two models

--- myapp/myapp/db/schema.rb
+++ myapp/myapp/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150511130524) do
+ActiveRecord::Schema.define(version: 20150516102717) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -19,5 +19,18 @@ ActiveRecord::Schema.define(version: 20150511130524) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "tickets", force: :cascade do |t|
+    t.string   "title"
+    t.string   "descryption"
+    t.string   "priority"
+    t.string   "rank"
+    t.string   "status"
+    t.integer  "article_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "tickets", ["article_id"], name: "index_tickets_on_article_id"
 
 end
