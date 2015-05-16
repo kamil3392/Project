0f3250ab9e75c9f0a066d1131f87e53125d70aa9
Kamil Kapturski <kamil3392@o2.pl>
2015-05-16 12:37:36 +0000

    edit show tickets

--- myapp/myapp/app/controllers/tickets_controller.rb
+++ myapp/myapp/app/controllers/tickets_controller.rb
@@ -1,2 +1,13 @@
 class TicketsController < ApplicationController
+    def create
+    @article = Article.find(params[:article_id])
+      @ticket = @article.tickets.create(ticket_params)
+    redirect_to article_path(@article)
+  end
+ 
+  private
+  def ticket_params
+    params.require(:ticket).permit(:title, :description)
+    end
 end
+
