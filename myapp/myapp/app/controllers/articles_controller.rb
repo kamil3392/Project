4bcb45ceb8230a2608258c5b5f7a928e70f200a3
Kamil Kapturski <kamil3392@o2.pl>
2015-05-18 12:50:29 +0000

    add page tickets

--- myapp/myapp/app/controllers/articles_controller.rb
+++ myapp/myapp/app/controllers/articles_controller.rb
@@ -1,5 +1,4 @@
 class ArticlesController < ApplicationController
- 
   def index
     @articles = Article.all
  end
@@ -21,14 +20,12 @@ def create
     render 'new'
   end
 end
- 
 def destroy
     @article = Article.find(params[:id])
     @article.destroy
  
     redirect_to articles_path
 end
-  
 private
   def article_params
     params.require(:article).permit(:title, :text)
--- myapp/myapp/app/controllers/tickets_controller.rb
+++ myapp/myapp/app/controllers/tickets_controller.rb
@@ -1,13 +1,16 @@
 class TicketsController < ApplicationController
+  def index
+    @article = Article.find(params[:article_id])
+  end
     def create
     @article = Article.find(params[:article_id])
       @ticket = @article.tickets.create(ticket_params)
     redirect_to article_path(@article)
   end
- 
   private
   def ticket_params
-    params.require(:ticket).permit(:title, :description)
+    params.require(:ticket).permit(:title,:descryption,:priority,:rank,:status)
     end
+
 end
 
