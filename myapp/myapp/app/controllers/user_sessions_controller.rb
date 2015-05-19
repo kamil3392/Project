37d42304260a4f0c7da72ec4ad52a16e7a6bbe6f
Kamil Kapturski <kamil3392@o2.pl>
2015-05-19 20:47:36 +0000

    changing application_controller

--- myapp/myapp/app/controllers/user_sessions_controller.rb
+++ myapp/myapp/app/controllers/user_sessions_controller.rb
@@ -1,74 +1,41 @@
 class UserSessionsController < ApplicationController
-  before_action :set_user_session, only: [:show, :edit, :update, :destroy]
-
-  # GET /user_sessions
-  # GET /user_sessions.json
-  def index
-    @user_sessions = UserSession.all
-  end
-
-  # GET /user_sessions/1
-  # GET /user_sessions/1.json
-  def show
-  end
 
   # GET /user_sessions/new
+  # GET /user_sessions/new.xml
   def new
     @user_session = UserSession.new
-  end
-
-  # GET /user_sessions/1/edit
-  def edit
-  end
-
-  # POST /user_sessions
-  # POST /user_sessions.json
-  def create
-    @user_session = UserSession.new(user_session_params)
 
     respond_to do |format|
-      if @user_session.save
-        format.html { redirect_to @user_session, notice: 'User session was successfully created.' }
-        format.json { render :show, status: :created, location: @user_session }
-      else
-        format.html { render :new }
-        format.json { render json: @user_session.errors, status: :unprocessable_entity }
-      end
+      format.html # new.html.erb
+      format.xml  { render :xml => @user_session }
     end
   end
 
-  # PATCH/PUT /user_sessions/1
-  # PATCH/PUT /user_sessions/1.json
-  def update
+  # POST /user_sessions
+  # POST /user_sessions.xml
+  def create
+    @user_session = UserSession.new(params[:user_session])
+
     respond_to do |format|
-      if @user_session.update(user_session_params)
-        format.html { redirect_to @user_session, notice: 'User session was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user_session }
+      if @user_session.save
+        format.html { redirect_to(:users, :notice => 'Login Successful') }
+        format.xml  { render :xml => @user_session, :status => :created, :location => @user_session }
       else
-        format.html { render :edit }
-        format.json { render json: @user_session.errors, status: :unprocessable_entity }
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @user_session.errors, :status => :unprocessable_entity }
       end
     end
   end
 
   # DELETE /user_sessions/1
-  # DELETE /user_sessions/1.json
+  # DELETE /user_sessions/1.xml
   def destroy
+    @user_session = UserSession.find
     @user_session.destroy
+
     respond_to do |format|
-      format.html { redirect_to user_sessions_url, notice: 'User session was successfully destroyed.' }
-      format.json { head :no_content }
+      format.html { redirect_to(:users, :notice => 'Goodbye!') }
+      format.xml  { head :ok }
     end
   end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user_session
-      @user_session = UserSession.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def user_session_params
-      params.require(:user_session).permit(:username, :password)
-    end
 end
--- myapp/myapp/config/routes.rb
+++ myapp/myapp/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  LoginApp::Application.routes.draw do
+    resources :users, :user_sessions
+    match 'login' => 'user_sessions#new', :as => :login
+    match 'logout' => 'user_sessions#destroy', :as => :logout
+end 
   
   resources :users
   resources :articles do
